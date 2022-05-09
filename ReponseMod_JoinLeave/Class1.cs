b51d775e3166c61f7105747243e45ca692782683
Reponse Mod <cinarbenliiphone@hotmail.com>
2022-05-09 20:27:00 +0300

    Proje dosyası ekle.

---
+++ ReponseMod_JoinLeave/Class1.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ReponseMod_JoinLeave
+{
+    public class Class1 : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            base.Load();
+            U.Events.OnPlayerConnected += Join;
+            U.Events.OnPlayerDisconnected += Leave;
+        }
+
+
+        private void Leave(UnturnedPlayer player)
+        {
+            Message($"<color=red>{player.CharacterName}</color> <color=orange>[<color=white>+</color>]</color>");
+        }
+
+        private void Join(UnturnedPlayer player)
+        {
+            Message($"<color=green>{player.CharacterName}</color> <color=orange>[<color=white>+</color>]</color>");
+        }
+
+        public void Message(string Message)
+        {
+            ChatManager.serverSendMessage(Message, Color.white, null, null, EChatMode.SAY, Configuration.Instance.ServerLogo, true);
+        }
+    }
+}
---
+++ ReponseMod_JoinLeave/Config.cs
@@ -0,0 +1,13 @@
+﻿using Rocket.API;
+
+namespace ReponseMod_JoinLeave
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string ServerLogo;
+        public void LoadDefaults()
+        {
+            ServerLogo = "https://media.discordapp.net/attachments/959142220366237796/962008357990977626/122.png";
+        }
+    }
+}
